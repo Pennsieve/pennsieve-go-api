dfdd23cc9d13375b9ee8770859acd5650a8bc84e
Michael Uftring <michael.uftring@gmail.com>
2023-06-06 12:35:31 -0400

    remove Ingest Service references (comment out did not work)

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -15,7 +15,6 @@ resource "aws_apigatewayv2_api" "upload-service-gateway" {
     publishing_service_lambda_arn = data.terraform_remote_state.publishing_service.outputs.service_lambda_arn,
     datasets_service_lambda_arn = data.terraform_remote_state.datasets_service.outputs.service_lambda_arn,
     packages_service_lambda_arn = data.terraform_remote_state.packages_service.outputs.service_lambda_arn,
-#    ingest_service_lambda_arn = data.terraform_remote_state.ingest_service.outputs.service_lambda_arn,
     user_pool_2_client_id = data.terraform_remote_state.authentication_service.outputs.user_pool_2_client_id,
     user_pool_endpoint = "https://${var.user_pool_endpoint}"
     token_pool_client_id = data.terraform_remote_state.authentication_service.outputs.token_pool_client_id,
