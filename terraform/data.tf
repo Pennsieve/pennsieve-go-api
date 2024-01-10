86c79c1de72d23ba5762cdfff1cd25fbeea4f13d
John Frommeyer <johnfr@seas.upenn.edu>
2024-01-10 12:54:36 -0500

    Add readme-service to Gateway

--- terraform/data.tf
+++ terraform/data.tf
@@ -166,3 +166,15 @@ data "terraform_remote_state" "rehydration_service" {
     profile = var.aws_account
   }
 }
+
+# Import Readme Service
+data "terraform_remote_state" "readme_service" {
+  backend = "s3"
+
+  config = {
+    bucket  = "${var.aws_account}-terraform-state"
+    key     = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/${var.environment_name}/readme-service/terraform.tfstate"
+    region  = "us-east-1"
+    profile = var.aws_account
+  }
+}
