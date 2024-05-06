737c92072f8777531b1428ba49e7fc0076c33f4b
Edmore Moyo <etmoyo@gmail.com>
2024-05-06 15:37:28 -0400

    adds compute node service endpoints

--- terraform/data.tf
+++ terraform/data.tf
@@ -190,3 +190,15 @@ data "terraform_remote_state" "account_service" {
     profile = var.aws_account
   }
 }
+
+# Import Compute Node Service
+data "terraform_remote_state" "compute_node_service" {
+  backend = "s3"
+
+  config = {
+    bucket  = "${var.aws_account}-terraform-state"
+    key     = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/${var.environment_name}/compute-node-service/terraform.tfstate"
+    region  = "us-east-1"
+    profile = var.aws_account
+  }
+}
