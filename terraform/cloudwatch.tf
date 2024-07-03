18905db3e943d88e6bf6c4f856cf28470ed21925
Michael Uftring <michael.uftring@gmail.com>
2024-07-03 11:56:48 -0400

    comment out cloudwatch log group creation for authorizer lambda

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,14 +1,14 @@
 // Create log group for upload lambda.
-resource "aws_cloudwatch_log_group" "authorizer_lambda_loggroup" {
-  name              = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}"
-  retention_in_days = 30
-  tags = local.common_tags
-}
+# resource "aws_cloudwatch_log_group" "authorizer_lambda_loggroup" {
+#   name              = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}"
+#   retention_in_days = 30
+#   tags = local.common_tags
+# }
 
 // Send logs from upload trigger lambda to datadog
 resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_group_subscription" {
-  name            = "${aws_cloudwatch_log_group.authorizer_lambda_loggroup.name}-subscription"
-  log_group_name  = aws_cloudwatch_log_group.authorizer_lambda_loggroup.name
+  name            = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}-subscription"
+  log_group_name  = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}"
   filter_pattern  = ""
   destination_arn = data.terraform_remote_state.region.outputs.datadog_delivery_stream_arn
   role_arn        = data.terraform_remote_state.region.outputs.cw_logs_to_datadog_logs_firehose_role_arn
