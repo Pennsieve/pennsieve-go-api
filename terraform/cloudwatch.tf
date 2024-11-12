7ae7dc4b7be26b843b50e340b2e1ef16f408b163
Edmore Moyo <etmoyo@gmail.com>
2024-11-12 13:58:08 -0500

    reverts datadog changes

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -6,10 +6,10 @@
 # }
 
 // Send logs from authorizer lambda to datadog
-resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_group_subscription" {
-  name            = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}-subscription"
-  log_group_name  = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}"
-  filter_pattern  = ""
-  destination_arn = data.terraform_remote_state.region.outputs.datadog_delivery_stream_arn
-  role_arn        = data.terraform_remote_state.region.outputs.cw_logs_to_datadog_logs_firehose_role_arn
-}
+# resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_group_subscription" {
+#   name            = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}-subscription"
+#   log_group_name  = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}"
+#   filter_pattern  = ""
+#   destination_arn = data.terraform_remote_state.region.outputs.datadog_delivery_stream_arn
+#   role_arn        = data.terraform_remote_state.region.outputs.cw_logs_to_datadog_logs_firehose_role_arn
+# }
