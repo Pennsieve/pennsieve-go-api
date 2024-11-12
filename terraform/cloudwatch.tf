b7f943d72806acf366c1fa6f662f91ed65c4b20f
Edmore Moyo <etmoyo@gmail.com>
2024-11-12 14:06:39 -0500

    updates comments

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,4 +1,4 @@
-// Create log group for upload lambda.
+// Create log group for authorizer lambda.
 # resource "aws_cloudwatch_log_group" "authorizer_lambda_loggroup" {
 #   name              = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}"
 #   retention_in_days = 30
@@ -6,6 +6,10 @@
 # }
 
 // Send logs from authorizer lambda to datadog
+// Currently the build fails if the block above is included as a log group already exists
+// Once the state is fixed (i.e. manually created resources deleted),
+// the code below should be updated to reference the log group created in the block above, and
+// not reference the created lambda directly
 # resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_group_subscription" {
 #   name            = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}-subscription"
 #   log_group_name  = "/aws/lambda/${aws_lambda_function.authorizer_lambda.function_name}"
