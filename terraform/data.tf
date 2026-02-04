c2d1a1d26474866d8e9bb0812523153c95a4c6cc
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2026-02-04 13:04:45 -0500

    remove from tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -143,18 +143,6 @@ data "terraform_remote_state" "import_service" {
   }
 }
 
-# Import Integration Service
-data "terraform_remote_state" "integration_service" {
-  backend = "s3"
-
-  config = {
-    bucket  = "${var.aws_account}-terraform-state"
-    key     = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/${var.environment_name}/integration-service/terraform.tfstate"
-    region  = "us-east-1"
-    profile = var.aws_account
-  }
-}
-
 # Import Rehydration Service
 data "terraform_remote_state" "rehydration_service" {
   backend = "s3"
@@ -203,17 +191,6 @@ data "terraform_remote_state" "github_service" {
   }
 }
 
-# Import Compute Node Service
-data "terraform_remote_state" "compute_node_service" {
-  backend = "s3"
-
-  config = {
-    bucket  = "${var.aws_account}-terraform-state"
-    key     = "aws/${data.aws_region.current_region.name}/${var.vpc_name}/${var.environment_name}/compute-node-service/terraform.tfstate"
-    region  = "us-east-1"
-    profile = var.aws_account
-  }
-}
 
 # Import App Deploy Service
 data "terraform_remote_state" "app_deploy_service" {
